6232da768a1c8a9e7152c2721e65c915ebeefaad
Ramez Mina <rtmmina@hotmail.com>
2020-08-19 08:51:24 -0500

    Added generic implementation for generic interface and unit tests for it. Not pluged into the service, so it is just a poc.

---
+++ ServiceLogic/Implementations/Calculator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ServiceLogic.Interfaces;
+using DataAccess.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace ServiceLogic.Implementations
+{
+    public class Calculator<T> : ICalculator<T>
+    {
+        private readonly ILog _log;
+        private readonly IConfiguration _configuration;
+
+        public Calculator(ILog log, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _log = log;
+        }
+
+        public T Add(T firstNmumber, T secondNumber)
+        {
+            try
+            {
+                var value = Operators.AddObject(firstNmumber, secondNumber);
+                _log.Info($"Add (Doubles) method for {firstNmumber} and {secondNumber} with result {value}, was called at {DateTime.Now}{Environment.NewLine}");
+                return (T)value;
+            }
+            catch (Exception ex)
+            {
+                _log.Error($"Exception has occured with method Add (Doubles) at {DateTime.Now}{Environment.NewLine}{ex.Message}{Environment.NewLine}");
+                object rVal = 0;
+                return (T)rVal;
+            }
+        }
+
+        public double Divide(T firstNmumber, T secondNumber)
+        {
+            try
+            {                
+                if (Convert.ToInt32(secondNumber) != 0)
+                {
+                    var value = Operators.DivideObject(firstNmumber, secondNumber);
+                    _log.Info($"Divide (Doubles) method for {firstNmumber} and {secondNumber} with result {value}, was called at {DateTime.Now}{Environment.NewLine}");
+                    return (double)value;
+                }
+                else
+                {
+                    _log.Error($"User has attempted to Divide by zero at {DateTime.Now}{Environment.NewLine}{Environment.NewLine}");
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error($"Exception has occured with method Divide (Doubles) at {DateTime.Now}{Environment.NewLine}{ex.Message}{Environment.NewLine}");
+                return 0;
+            }
+        }
+
+        public T Multiply(T firstNmumber, T secondNumber)
+        {
+            try
+            {
+                var value = Operators.MultiplyObject(firstNmumber, secondNumber);
+                _log.Info($"Multiply (Doubles) method for {firstNmumber} and {secondNumber} with result {value}, was called at {DateTime.Now}{Environment.NewLine}");
+                return (T)value;
+            }
+            catch (Exception ex)
+            {
+                _log.Error($"Exception has occured with method Multiply (Doubles) at {DateTime.Now}{Environment.NewLine}{ex.Message}{Environment.NewLine}");
+                object rVal = 0;
+                return (T)rVal;
+            }
+        }
+
+        public T Sub(T firstNmumber, T secondNumber)
+        {
+            try
+            {
+                var value = Operators.SubtractObject(firstNmumber, secondNumber);
+                _log.Info($"Sub (Doubles) method for {firstNmumber} and {secondNumber} with result {value}, was called at {DateTime.Now}{Environment.NewLine}");
+                return (T)value;
+            }
+            catch (Exception ex)
+            {
+                _log.Error($"Exception has occured with method Sub (Doubles) at {DateTime.Now}{Environment.NewLine}{ex.Message}{Environment.NewLine}");
+                object rVal = 0;
+                return (T)rVal;
+            }
+        }
+    }
+}
---
+++ UnitTest/TestCalculatorGenericsForDoubles.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLogic.Interfaces;
+using ServiceLogic.Implementations;
+using System.Transactions;
+using DataAccess.Interfaces;
+using DataAccess.Implementations;
+using Microsoft.Extensions.Configuration;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestCalculatorGenericsForDoubles
+    {
+        private ICalculator<double> _calculator;
+        private readonly IConfiguration _configuration;
+
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            return config;
+        }
+        public TestCalculatorGenericsForDoubles()
+        {
+            _configuration = InitConfiguration();
+            ILog _log = new Log(_configuration);
+            _calculator = new Calculator<double>(_log, _configuration);
+        }
+
+        public TestCalculatorGenericsForDoubles(ICalculator<double> doubleCalculator)
+        {
+            _configuration = InitConfiguration();
+            _calculator = doubleCalculator;
+        }
+
+        #region Add tests
+        [TestMethod]
+        public void Add_TwoPositive()
+        {
+            //Act
+            var actual = _calculator.Add(5.2, 6.33);
+            var expected = (double)5.2 + 6.33;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_TwoNegative()
+        {
+            //Act
+            var actual = _calculator.Add(-1.13, -5.37);
+            var expected = (double)-1.13 + (-5.37);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_OnePositiveOneNegative()
+        {
+            //Act
+            var actual = _calculator.Add(3.97, -11.75);
+            var expected = (double)3.97 + (-11.75);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+
+        #region Sub tests
+        [TestMethod]
+        public void Sub_TwoPositive()
+        {
+            //Act
+            var actual = _calculator.Sub(5.13, 6.21);
+            var expected = (double)5.13 - 6.21;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Sub_TwoNegative()
+        {
+            //Act
+            var actual = _calculator.Sub(-1.85, -5.78);
+            var expected = (double)-1.85 - (-5.78);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Sub_OnePositiveOneNegative()
+        {
+            //Act
+            var actual = _calculator.Sub(3.45, -11.55);
+            var expected = (double)3.45 - (-11.55);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+
+        #region Multiply tests
+        [TestMethod]
+        public void Multiply_TwoPositive()
+        {
+            //Act
+            var actual = _calculator.Multiply(5.16, 6.89);
+            var expected = (double)5.16 * 6.89;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiply_TwoNegative()
+        {
+            //Act
+            var actual = _calculator.Multiply(-1.99, -5.67);
+            var expected = (double)-1.99 * (-5.67);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiply_OnePositiveOneNegative()
+        {
+            //Act
+            var actual = _calculator.Multiply(3.96, -11.14);
+            var expected = (double)3.96 * (-11.14);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+
+        #region Divide tests
+        [TestMethod]
+        public void Divide_TwoPositive()
+        {
+            //Act
+            var actual = _calculator.Divide(5.11, 6.77);
+            var expected = (double)5.11 / 6.77;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_TwoNegative()
+        {
+            //Act
+            var actual = _calculator.Divide(-1.37, -5.97);
+            var expected = (double)-1.37 / -5.97;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_OnePositiveOneNegative()
+        {
+            //Act
+            var actual = _calculator.Divide(3.39, -11.93);
+            var expected = (double)3.39 / -11.93;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_DivideByZero()//Currently the service would return a zero value, and log the exception.
+        {
+            //Act
+            var actual = _calculator.Divide(-3.69, 0.0);
+
+            //Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        #endregion
+    }
+}
